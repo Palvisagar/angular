06856eddb948d6e5448a4a2eb6c1844073a11a1c
palvisagar <Pallvisagar.code@gmail.com>
2022-12-03 23:45:36 +0530

    second commit

--- educative/src/app/admin/exam/exam.component.ts
+++ educative/src/app/admin/exam/exam.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-exam',
   templateUrl: './exam.component.html',
   styleUrls: ['./exam.component.css']
 })
-export class ExamComponent {
+export class ExamComponent implements OnInit{
+  
+  examForm={
+    name:'',
+    email:'',
+    password:''
+  }
 
 }
